65a0ef325099d455efaf5560e7d4f726d59d74ef
Tadashi <kaunghtetaung374@gmail.com>
2025-05-13 11:45:29 +0630

    add result

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,21 @@
 // src/App.jsx
-import React from 'react';
-import Boy from './Boy';
-import Girl from './Girl';
-import LoginPage from './pages/Login';
-import Votes from './pages/Votes';
-import ProtectedRoute from './components/ProtectedRoute';
-import { BrowserRouter,Routes,Route} from 'react-router-dom';
+import React from "react";
+import Boy from "./Boy";
+import Girl from "./Girl";
+
+import Votes from "./pages/Votes";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <BrowserRouter>
-        <Routes>
-          <Route
-              path="/login"
-              element={
-                 <LoginPage/>
-               }
-           />
-             
-          <Route
-              path="/"
-              element={
-                   <ProtectedRoute>
-                      <Boy/> 
-                   </ProtectedRoute>
-                                
-              
-               }
-           />
-           <Route
-              path="/girl"
-              element={
-                  <ProtectedRoute>
-                      <Girl/> 
-                   </ProtectedRoute>
-                                  
-               
-               }
-           />
-             
-            <Route
-              path="/votes"
-              element={
-                 
-                       <ProtectedRoute>
-                      <Votes/> 
-                   </ProtectedRoute>
-                                   
-               
-               }
-           />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Boy />} />
+        <Route path="/girl" element={<Girl />} />
+
+        <Route path="/votes" element={<Votes />} />
+      </Routes>
     </BrowserRouter>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export const Navbar=()=>{
-    return(
-         <nav>
-                <div className="logo">
-                    <img src="./images/logo.webp" alt="" width={50} height={50} style={{lineHeight:50}}/>
-                </div>
-                <ul id="menuList">
-                    <li><Link to="/">Boys</Link></li>
-                    <li><Link to="/girl">Girls</Link></li>
-                    <li>
-                      <Link to="/votes">
-                         <img src="./images/ballot-box.png" alt=""width={30}height={30} />
-                      </Link>
-                    </li>
-                </ul>      
-            </nav>
-    )
-}
+export const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <img
+          src="./images/logo.webp"
+          alt=""
+          width={50}
+          height={50}
+          style={{ lineHeight: 50 }}
+        />
+      </div>
+      <ul id="menuList">
+        <li>
+          <Link to="/">Boys</Link>
+        </li>
+        <li>
+          <Link to="/girl">Girls</Link>
+        </li>
+        <li>
+          <Link to="https://vote-result.netlify.app/">Live Result</Link>
+        </li>
+        <li>
+          <Link to="/votes">
+            <img src="./images/ballot-box.png" alt="" width={30} height={30} />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
